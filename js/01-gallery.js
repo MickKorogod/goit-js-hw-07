82ffd63c4147b628df28ec8b8f1b11347aff2469
MickKorogod <korogod1995@gmail.com>
2022-07-18 00:50:57 +0300

    tsk 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const createGallery = galleryItems.map(item => {
+ return `<a class="gallery__link" href="${item.preview}"> <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"
+    /> </a>`
+}).join('')
+console.log(createGallery)
+const container = document.querySelector(".gallery")
+container.insertAdjacentHTML("afterbegin", createGallery);    
+
+const modal = document.querySelector(".modal")
+function galleryHandler(e) {
+  e.preventDefault()
+  const dataValue = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <div class="modal">
+  <img src = "${dataValue}">
+    </div>`
+    
+  )
+  instance.show()
+  
+}
+
+container.addEventListener('click', galleryHandler)
+
+function onEscClick(event) {
+            if (event.code === 'Escape') {
+                instance.close()
+            }
+        }
+modal.removeEventListener('keydown', onEscClick);
