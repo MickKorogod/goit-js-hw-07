e97d90cc8487c117c2a1b627ebeb9ce0208b4fb2
MickKorogod <korogod1995@gmail.com>
2022-07-18 23:36:14 +0300

    tsk2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const createGallery = galleryItems.map(item => {
+ return `<a class="gallery__link" href="${item.original}"> <img class="gallery__image" src="${item.preview}" alt="${item.description}"
+    /> </a>`
+}).join('')
+console.log(createGallery)
+const container = document.querySelector(".gallery")
+container.insertAdjacentHTML("afterbegin", createGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
