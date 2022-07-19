5f3cbe70c979af7e33487235ce457c73870441de
MickKorogod <korogod1995@gmail.com>
2022-07-19 20:09:06 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,34 @@
- import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const createGallery = galleryItems.map(item => {
  return `<a class="gallery__link" href="${item.preview}"> <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"
     /> </a>`
 }).join('')
-console.log(createGallery)
-const container = document.querySelector(".gallery")
-container.insertAdjacentHTML("afterbegin", createGallery);    
 
-const modal = document.querySelector(".modal")
-function galleryHandler(e) {
-  e.preventDefault()
-  const dataValue = e.target.dataset.source;
-  const instance = basicLightbox.create(`
-    <div class="modal">
-  <img src = "${dataValue}">
-    </div>`
-    
-  )
-  instance.show()
-  
-}
+const container = document.querySelector(".gallery")
+
+container.insertAdjacentHTML("afterbegin", createGallery); 
 
 container.addEventListener('click', galleryHandler)
 
-function onEscClick(event) {
-            if (event.code === 'Escape') {
-                instance.close()
-            }
-        }
-modal.removeEventListener('keydown', onEscClick);
+function galleryHandler(e) {
+  e.preventDefault()
+
+  const dataValue = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+  <img src = "${dataValue}">`,
+    {
+      onShow: () => window.addEventListener('keydown', onEscClick),
+      onclose: () => window.removeEventListener('keydown', onEscClick)
+    }
+  );
+
+  instance.show()
+
+  function onEscClick(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
